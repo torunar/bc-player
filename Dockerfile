10b555e240d3db2d10d8875a93c0b14d81eb4999
Mikhail Shchekotov <torunar@gmail.com>
2024-06-17 17:35:27 +0200

    Get rid of PHP backend

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM php:8.1.15-cli-alpine
-
-COPY . /srv
-WORKDIR /srv
-
-CMD ["/bin/sh", "-c", "php -S 0.0.0.0:8080 -t /srv/public"]
--- template/player.php
+++
@@ -1,3 +0,0 @@
-<?php foreach (($queue ?? []) as $track): ?>
-    <?php require __DIR__ . '/playlistItem.php' ?>
-<?php endforeach; ?>
